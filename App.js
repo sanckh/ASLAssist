dffdb3990d3acc821a1bb2d374a419bc6fef6b64
ParkVra <VPark@student.fullsail.edu>
2022-05-26 00:49:47 -0400

    App title, username, password basic page layout

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>ASLAssist</Text>
+      <View style={{width: '60%', height: '5', backgroundColor: '0A2941'}}></View>
+
+      <Text style={{fontSize: 24, textAlign:'left', color: '414141', margin: 30}}>Username</Text>
+
+      <Text style={{width: '60%', height: '5', backgroundColor: 'transparent'}}> </Text>
+
+      <Text style={{fontSize: 24, textAlign:'left', color: '414141'}}>Password</Text>
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffdf9',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 10
   },
+  text: {
+    fontColor: '414141',
+    fontWeight: 'bold',
+    alignItems: 'center',
+    fontSize: 36
+  },
+  // inputText: {
+  //   fontColor: '414141',
+  //   fontSize: 24,
+  //   alignItems: 'left'
+  // }
 });
