74ecabe9242a2524f903c7820e278354c4c4fa57
sanckh <csutton@student.fullsail.edu>
2022-06-20 14:17:15 -0500

    began working on achievements

--- firebase.js
+++ firebase.js
@@ -4,7 +4,6 @@ import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
--- navigation/Achievements.js
+++ navigation/Achievements.js
@@ -1,5 +1,6 @@
 import { Text, View, ScrollView, StyleSheet, Alert, Linking } from 'react-native';
 import React, {useCallback} from 'react';
+import { List, Divider, TouchableRipple } from 'react-native-paper';
 import Header from '../components/Header';
 import BackButton from '../components/BackButton';
 import Background from '../components/Background';
@@ -8,16 +9,45 @@ import Paragraph from '../components/Paragraph';
 import Button from '../components/Button';
 
 export default function Achievement({navigation}){
-    return(
-        <Background>
-            <BackButton goBack = {navigation.goBack} />
-            <Logo/>
-            <Header>Achievements</Header>
-            <Paragraph>
-                This is where the list of all achievements will go.
-            </Paragraph>
 
-        </Background>
+  const getAchievements = () => {
+    //code to call firestore achievements master list
+  }
+    return(
+        <View style = {styles.containerView}>
+          <BackButton goBack = {navigation.goBack} />
+          <View>
+          <Logo/>
+          <Header>Achievements</Header>
+          
+          <View style = {styles.header}>
+            <Header>Profile</Header>
+          </View>
+
+        </View>
+            <View>
+              {/* display getAchievements */}
+            </View>
+
+            <View>
+            <List.Section
+              borderColor= 'black'
+            >
+              <List.Subheader>Recent Achievements</List.Subheader>
+              <List.Item title = "Achievement placeholder"
+              //make aa check mark for completed
+              left = {() => <List.Icon icon="check-decagram" />}
+              />
+
+            </List.Section>
+            <Divider/>
+            <List.Section>
+              <List.Subheader>Upcoming Achievements</List.Subheader>
+            </List.Section>
+            <Divider/>
+          </View>
+
+          </View>
     )
 }
 
@@ -40,5 +70,14 @@ const styles = StyleSheet.create({
       alignSelf: 'center', 
       padding: 5, 
       marginRight: 10
-    }
+    },
+    containerView: {
+      flex: 1,
+      padding: 10,
+      width: '100%',
+      maxWidth: 1000,
+      alignSelf: 'center',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
   });
