898e3b8544a163eb067323c3d41e7eaa15b7f50f
sanckh <csutton@student.fullsail.edu>
2022-06-28 13:51:40 -0500

    firebase issues

--- firebase.js
+++ firebase.js
@@ -1,29 +1,39 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore, collection, getDocs } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA1zo9ci0RXQYBNUooGtSK2b29OPxOC0GE",
-  authDomain: "aslassistfinal.firebaseapp.com",
-  databaseURL: "https://aslassistfinal-default-rtdb.firebaseio.com",
-  projectId: "aslassistfinal",
-  storageBucket: "aslassistfinal.appspot.com",
-  messagingSenderId: "380652280816",
-  appId: "1:380652280816:web:5f49bf6df4977cdb401b35",
-  //measurementId: "G-TYX10C1CBE"
+  apiKey: "AIzaSyDmuiN_oWQsMYtHmlMZzDJKFLx2muLS_ec",
+  authDomain: "asl-assist.firebaseapp.com",
+  projectId: "asl-assist",
+  storageBucket: "asl-assist.appspot.com",
+  messagingSenderId: "149243013990",
+  appId: "1:149243013990:web:7f117842ec0a235a2df6a7"
 };
 
 // Initialize Firebase
+export const app = initializeApp(firebaseConfig);
 
-const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
 
-const auth = getAuth(app);
+export const db = getFirestore(app);
 
-export { auth }
+  //collection reference
+  const colRef = collection(db, 'Achievements')
+
+  //get collection data
+  getDocs(colRef)
+    .then((snapshot) => {
+     let achievements = []
+     snapshot.docs.forEach((doc) => {
+      achievements.push({...doc.data(), id: doc.id})
+     })
+     console.log(achievements)
+    })
+    .catch(error => {
+      console.log(error.message)
+    })
