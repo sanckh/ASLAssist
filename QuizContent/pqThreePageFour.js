aec0dc3f6fc33b76eb619dec90aca32fbed4e0d7
ParkVra <VPark@student.fullsail.edu>
2022-07-17 17:37:32 -0400

    Practice Quiz Three UI

--- QuizContent/pqThreePageFour.js
+++ QuizContent/pqThreePageFour.js
@@ -1,4 +1,4 @@
-import { Text, View, ScrollView, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
 import Header from '../components/Header';
 import { useTheme } from '@react-navigation/native';
 import BackButton from '../components/BackButton';
@@ -11,7 +11,10 @@ import { theme } from '../core/theme';
 
 export default function LessonSix({navigation}){
     const {colors} = useTheme();
-
+    const wrongAnswer = () => {
+        Alert.alert('This answer is incorrect. \nPlease try again');
+    };
+    
     return(
         <Background>
             <BackButton goBack = {navigation.goBack} />
@@ -24,9 +27,14 @@ export default function LessonSix({navigation}){
                     uri: 'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Emergency%2Fhurry.gif?alt=media&token=74593331-c378-4899-959b-0e81f8d585d2'
                 }}>
                 </Image>
+                <View>
+                <ProgressBar style = {{width: 200, marginTop: 15}} progress={1.00} color={"lightgreen"} />
+                </View>
+
                 <Text style={styles.text}>What sign is this?</Text>
                 <View style={styles.screen}>
             <Button
+            onPress = {() => navigation.navigate('QuizCompletion')}
             mode = "contained"
             style = {{backgroundColor: '#daeaf6', 
                 width: 150,
@@ -40,6 +48,7 @@ export default function LessonSix({navigation}){
             </Button>
 
             <Button
+            onPress={()=>wrongAnswer()}
             mode = "contained"
             style = {{backgroundColor: '#daeaf6', 
                 width: 150,
@@ -53,6 +62,7 @@ export default function LessonSix({navigation}){
             </View>
             <View style={styles.screen}>
             <Button
+            onPress={()=>wrongAnswer()}
             mode = "contained"
             style = {{backgroundColor: '#daeaf6', 
                 width: 150,
@@ -66,6 +76,7 @@ export default function LessonSix({navigation}){
             </Button>
 
             <Button
+            onPress={()=>wrongAnswer()}
             mode = "contained"
             style = {{backgroundColor: '#daeaf6', 
                 width: 150,
@@ -78,7 +89,7 @@ export default function LessonSix({navigation}){
             </Button>
 
             </View>
-            <View>
+            {/* <View>
                 <ProgressBar style = {{width: 200, marginTop: 15}} progress={1.00} color={"lightgreen"} />
             </View>
             <Button
@@ -89,7 +100,7 @@ export default function LessonSix({navigation}){
                 height: 50,}}
             >
                 Next Section
-            </Button>
+            </Button> */}
         </Background>
         
         
--- navigation/practiceQuizThree.js
+++ navigation/practiceQuizThree.js
@@ -1,4 +1,4 @@
-import { Text, View, ScrollView, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { Text, View, ScrollView, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
 import Header from '../components/Header';
 import { useTheme } from '@react-navigation/native';
 import BackButton from '../components/BackButton';
@@ -11,7 +11,10 @@ import { theme } from '../core/theme';
 
 export default function PracticeOne({navigation}){
     const {colors} = useTheme();
-
+    const wrongAnswer = () => {
+        Alert.alert('This answer is incorrect. \nPlease try again');
+    };
+    
     return(
         <Background>
             <BackButton goBack = {navigation.goBack} />
@@ -24,9 +27,14 @@ export default function PracticeOne({navigation}){
                     uri: 'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Emergency%2Fburn.gif?alt=media&token=d2506a0a-52c3-436e-b586-166f815ca88a'
                 }}>
                 </Image>
+                <View>
+                <ProgressBar style = {{width: 200, marginTop: 15}} progress={0.0} color={"lightgreen"} />
+                </View>
+
                 <Text style={styles.text}>What sign is this?</Text>
                 <View style={styles.screen}>
             <Button
+            onPress={()=>navigation.navigate('pqThreePageTwo')}
             mode = "contained"
             style = {{backgroundColor: '#daeaf6', 
                 width: 150,
@@ -40,6 +48,7 @@ export default function PracticeOne({navigation}){
             </Button>
 
             <Button
+            onPress={()=>wrongAnswer()}
             mode = "contained"
             style = {{backgroundColor: '#daeaf6', 
                 width: 150,
@@ -53,6 +62,7 @@ export default function PracticeOne({navigation}){
             </View>
             <View style={styles.screen}>
             <Button
+            onPress={()=>wrongAnswer()}
             mode = "contained"
             style = {{backgroundColor: '#daeaf6', 
                 width: 150,
@@ -66,6 +76,7 @@ export default function PracticeOne({navigation}){
             </Button>
 
             <Button
+            onPress={()=>wrongAnswer()}
             mode = "contained"
             style = {{backgroundColor: '#daeaf6', 
                 width: 150,
@@ -79,10 +90,10 @@ export default function PracticeOne({navigation}){
 
             </View>
 
-            <View>
+            {/* <View>
                 <ProgressBar style = {{width: 200, marginTop: 15}} progress={0.0} color={"lightgreen"} />
-            </View>
-            <Button
+            </View> */}
+            {/* <Button
             mode = "contained"
             onPress = {() => navigation.navigate('pqThreePageTwo')}
             style = {{backgroundColor: '#ecddfc', 
@@ -90,7 +101,7 @@ export default function PracticeOne({navigation}){
                 height: 50,}}
             >
                 Next Section
-            </Button>
+            </Button> */}
         </Background>
         
         
