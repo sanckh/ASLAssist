48a10d48646aaa0690615d89127b39ad82f2ab7e
Paulene Deodat <PKDeodat@student.fullsail.edu>
2022-08-02 15:32:40 -0400

    Numbers def shortened

--- navigation/Numbers.js
+++ navigation/Numbers.js
@@ -35,7 +35,7 @@ export default function LessonThree({navigation}){
                                 uri: 'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Numbers%2F1.png?alt=media&token=f61e110e-ff10-4340-8ed8-8d65a9e7eec1'
                             }}>
                             </Image>
-                        <Text style = {[styles.text, {color: colors.text}]}>The number 1 is signed by holding up your dominant hand in a fist, facing towards you, with your index finger sticking out. Don't confuse the number 1 for the letter I, where you stick out your pinkie finger from your fist, facing out.</Text>
+                        <Text style = {[styles.text, {color: colors.text}]}>The number 1 is signed by holding hand in fist, facing towards you, with your index finger sticking out.</Text>
                     </View>
                 </View>
 
@@ -47,7 +47,7 @@ export default function LessonThree({navigation}){
                                 uri:  'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Numbers%2F2.png?alt=media&token=a2a15a03-b934-4b26-b888-d9bec1b599ff'
                                                         }}>
                         </Image>
-                        <Text style = {[styles.text, {color: colors.text}]}>The number 2 is signed by holding up your dominant hand in a fist, facing towards you, with your index and middle fingers sticking out. The number 2 looks like the letter V, but this letter has the fist facing out.</Text>
+                        <Text style = {[styles.text, {color: colors.text}]}>The number 2 is signed by holding hand in fist, facing towards you, with your index and middle fingers sticking out.</Text>
                     </View>
 
                         <View style = {styles.imagePlacement}>
@@ -57,7 +57,7 @@ export default function LessonThree({navigation}){
                                 uri: 'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Numbers%2F3.png?alt=media&token=b0fcd366-48a5-4c49-a55a-57c9749bd0ec'
                             }}>
                             </Image>
-                        <Text style = {[styles.text, {color: colors.text}]}>The number 3 is signed by holding up your dominant hand in a fist, facing towards you, then sticking out your thumb, index and middle fingers.</Text>
+                        <Text style = {[styles.text, {color: colors.text}]}>The number 3 is signed by holding hand in fist, facing towards you, then sticking out your thumb, index and middle fingers.</Text>
                     </View>
                 </View>
 
@@ -69,7 +69,7 @@ export default function LessonThree({navigation}){
                                 uri:  'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Numbers%2F4.png?alt=media&token=bb9a9e0c-9fd1-44e6-91a6-302394e17ef8'
                                                         }}>
                         </Image>
-                        <Text style = {[styles.text, {color: colors.text}]}>The number 4 is signed by holding up your dominant hand in a fist, facing towards you, then sticking out your four fingers spread apart while your thumb is tucked in.</Text>
+                        <Text style = {[styles.text, {color: colors.text}]}>The number 4 is signed by holding hand in fist, facing towards you, sticking out your four fingers spread apart while your thumb is tucked in.</Text>
                     </View>
 
                         <View style = {styles.imagePlacement}>
@@ -91,7 +91,7 @@ export default function LessonThree({navigation}){
                                 uri:  'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Numbers%2F6.png?alt=media&token=4d37c6e3-d92b-4fc4-80bb-ad747a44b54e'
                                                         }}>
                         </Image>
-                        <Text style = {[styles.text, {color: colors.text}]}>To make the number six, open your hand and touch your thumb to your pinky finger.</Text>
+                        <Text style = {[styles.text, {color: colors.text}]}>To make the number 6, open your hand and touch your thumb to your pinky finger.</Text>
                     </View>
 
                         <View style = {styles.imagePlacement}>
@@ -101,7 +101,7 @@ export default function LessonThree({navigation}){
                                 uri: 'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Numbers%2F7.png?alt=media&token=5c55ee48-1d0c-4821-a421-849b5e184730'
                             }}>
                             </Image>
-                        <Text style = {[styles.text, {color: colors.text}]}>To make the number seven in sign language, touch your thumb to the finger next to your pinky finger.</Text>
+                        <Text style = {[styles.text, {color: colors.text}]}>To make the number 7 in sign language, touch your thumb to the finger next to your pinky finger.</Text>
                     </View>
                 </View>
 
@@ -123,7 +123,7 @@ export default function LessonThree({navigation}){
                                 uri: 'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Numbers%2F9.png?alt=media&token=703e2d76-1a2c-4ef0-8819-4eb8fb36801f'
                             }}>
                             </Image>
-                        <Text style = {[styles.text, {color: colors.text}]}>The number 9 is signed by holding up your open, dominant hand, facing out, then touching your thumb and index finger together. Make sure to flatten out your thumb and index a bit more.</Text>
+                        <Text style = {[styles.text, {color: colors.text}]}>The number 9 is signed by holding up hand facing out, touching your thumb and index finger together.</Text>
                     </View>
                 </View>
                 <View  style = {{flexDirection: 'row', marginTop: 10}}>
@@ -134,7 +134,7 @@ export default function LessonThree({navigation}){
                                 uri:  'https://firebasestorage.googleapis.com/v0/b/aslassistfinal.appspot.com/o/Numbers%2F10.png?alt=media&token=63392b0f-4c41-4490-bef7-5c83670c2f0a'
                                                         }}>
                         </Image>
-                        <Text style = {[styles.text, {color: colors.text}]}>The number 10 is signed by holding up your dominant hand in a fist, with thumb sticking up, then twisting your hand at the wrist back and forth.</Text>
+                        <Text style = {[styles.text, {color: colors.text}]}>The number 10 is signed by holding hand in fist, with thumb sticking up, twisting hand at the wrist back and forth.</Text>
                     </View>
                 </View>
                 </ScrollView>
